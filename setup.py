45723c9def6bdc464e3a196958bb5d58ec549c72
jp-96 <jp-96@outlook.jp>
2020-12-30 21:32:51 +0900

    console

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name="mbedsetup",
-	version="0.1",
+	version="0.1.1",
     author='jp.rad',
     author_email='jp.rad@outlook.jp',
     license='MIT',
     packages=find_packages(),
+    package_data={'': ['*.patch']},
+    entry_points={
+        'console_scripts': [
+            'mbedsetup=mbedsetup:main',
+        ],
+    },
     install_requires=[
         'patch',
         'jsonschema',
